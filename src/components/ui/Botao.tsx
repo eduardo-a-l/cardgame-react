48dc14194e5b78b2bbf8610852fa45de0b18ec61
Eduardo-de-Almeida-Lage <cc25127@g.unicamp.br>
2026-08-22 16:34:05 -0300

    feat: Melhora componente Botao

--- src/components/ui/Botao.tsx
+++ src/components/ui/Botao.tsx
@@ -1,6 +1,9 @@
-interface BotaoProps {
-  texto: string;
-  onClick?: () => void;
+import { ComponentProps } from "react";
+import { Icone, NomeIcone } from "./Icone";
+
+interface BotaoProps extends ComponentProps<"button"> {
+  texto?: string;
+  nomeIcone?: NomeIcone;
   corDeFundo?: string;
   corDaBorda?: string;
   corDoTexto?: string;
@@ -8,22 +11,27 @@ interface BotaoProps {
 
 export function Botao({
   texto,
-  onClick,
+  nomeIcone,
   corDeFundo = "#21366B",
   corDaBorda = "#C8911A",
   corDoTexto = "#FFFFFF",
+  className = "",
+  style,
+  ...props
 }: BotaoProps) {
   return (
     <button
-      onClick={onClick}
       style={{
         backgroundColor: corDeFundo,
         color: corDoTexto,
         borderColor: corDaBorda,
-        borderRadius: "5px",
+        ...style,
       }}
+      className={`flex items-center gap-3 px-5 py-2.5 border-2 rounded-xl ${className}`}
+      {...props}
     >
-      {texto}
+      {nomeIcone && <Icone nome={nomeIcone} tamanho={20} />}
+      {texto && <span>{texto}</span>}
     </button>
   );
 }
