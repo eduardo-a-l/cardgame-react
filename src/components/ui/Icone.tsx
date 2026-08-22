6270f955691b8086c318a3158ec67b60755ecab2
Eduardo-de-Almeida-Lage <cc25127@g.unicamp.br>
2026-08-22 16:50:50 -0300

    feat: adiciona mais icones à componente icone

--- src/components/ui/Icone.tsx
+++ src/components/ui/Icone.tsx
@@ -1,12 +1,26 @@
 import IconeLeaderboard from "@/assets/icons/leaderboard.svg";
 import IconeUsuario from "@/assets/icons/user.svg";
 import IconeConfiguracoes from "@/assets/icons/settings.svg";
+import IconeJogar from "@/assets/icons/play.svg";
+import IconeInventario from "@/assets/icons/package.svg";
+import IconeBaralho from "@/assets/icons/deck.svg";
+import IconeLoja from "@/assets/icons/shop.svg";
+import IconeVoltar from "@/assets/icons/back.svg";
+import IconeNovo from "@/assets/icons/plus.svg";
+import IconeParaBaixo from "@/assets/icons/down.svg";
 import { ComponentProps } from "react";
 
 const icones = {
   leaderboard: IconeLeaderboard,
   usuario: IconeUsuario,
   configuracoes: IconeConfiguracoes,
+  jogar: IconeJogar,
+  inventario: IconeInventario,
+  baralho: IconeBaralho,
+  loja: IconeLoja,
+  voltar: IconeVoltar,
+  novo: IconeNovo,
+  paraBaixo: IconeParaBaixo,
 };
 
 export type NomeIcone = keyof typeof icones;
