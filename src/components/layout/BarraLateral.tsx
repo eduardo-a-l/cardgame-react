048400f94efe61ceb1d54528b23efa0c3971b2fe
Eduardo-de-Almeida-Lage <cc25127@g.unicamp.br>
2026-08-22 15:57:23 -0300

    feat: Adiciona componente BarraLateral

---
+++ src/components/layout/BarraLateral.tsx
@@ -0,0 +1,28 @@
+import { BotaoIcone } from "@/components/ui/BotaoIcone";
+
+interface BarraLateralProps {
+  corDeFundo?: string;
+  corDaBorda?: string;
+}
+
+export function BarraLateral({
+  corDeFundo = "#C8911A",
+  corDaBorda = "#000000",
+}: BarraLateralProps) {
+  return (
+    <aside
+      className="w-16 h-screen flex flex-col justify-between items-center py-4 border-r"
+      style={{
+        backgroundColor: corDeFundo,
+        borderColor: corDaBorda,
+      }}
+    >
+      <div className="flex flex-col gap-3">
+        <BotaoIcone nomeIcone="leaderboard" onClick={() => ""} />
+        <BotaoIcone nomeIcone="usuario" onClick={() => ""} />
+      </div>
+
+      <BotaoIcone nomeIcone="configuracoes" onClick={() => ""} />
+    </aside>
+  );
+}
