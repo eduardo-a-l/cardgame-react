ef48fb694e9dd144eaf22c1513bf227ae667ef70
Eduardo-de-Almeida-Lage <cc25127@g.unicamp.br>
2026-08-30 20:44:10 -0300

    feat: Implementa tela de configurações

--- src/app/configuracoes/page.tsx
+++ src/app/configuracoes/page.tsx
@@ -1,14 +1,42 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { Botao } from "@/components/ui/Botao";
 
 export default function TelaConfiguracoes() {
+  const router = useRouter();
+
   return (
-    <main className="p-10 h-screen bg-[#1B1B2F]">
-      <div>
-        <h1 className="text-white">Configurações</h1>
+    <main className="relative h-screen bg-[#1B1B2F] overflow-hidden">
+      <div className="absolute m-10">
+        <Botao
+          texto="Voltar"
+          nomeIcone="voltar"
+          tamanhoIcone={40}
+          corDoTexto="#FFFFFF"
+          corDeFundo="transparent"
+          corDaBorda="transparent"
+          className="px-0 py-0 text-3xl"
+          onClick={() => router.push("/")}
+        />
       </div>
-      <div className="vgap-10">
-        <Botao texto="Deletar Conta" />
-        <Botao texto="Sair da Conta" />
+
+      <h1 className="pt-20 text-center text-4xl text-white">Configurações</h1>
+
+      <div className="absolute left-15 bottom-15 flex flex-col gap-6">
+        <Botao
+          texto="Deletar Conta"
+          corDoTexto="#FFFFFF"
+          className="px-20 py-6 text-3xl"
+          onClick={() => {}}
+        />
+
+        <Botao
+          texto="Sair da Conta"
+          corDoTexto="#FFFFFF"
+          className="px-20 py-6 text-3xl"
+          onClick={() => {}}
+        />
       </div>
     </main>
   );
